41975cfab78de63f5daaa621387f434b512f4acd
Brian Camacho <hello@briancamacho.me>
2024-08-06 16:43:10 +0200

    fmt

--- flake.nix
+++ flake.nix
@@ -2,43 +2,39 @@
   description = "C++ library for fetching git-lfs files";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-   outputs =
-    { self, nixpkgs }:
+  outputs = { self, nixpkgs }:
     let
       inherit (self) outputs;
-      systems = [
-        "aarch64-linux"
-        "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
+      systems =
+        [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
+    in {
+      packages = forAllSystems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
           default = pkgs.stdenv.mkDerivation {
-      name = "git-lfs-fetch";
-      src = self;
-      buildInputs = [ pkgs.clang pkgs.gnumake pkgs.curl pkgs.nlohmann_json pkgs.libgit2 ];
-      buildPhase = ''
-        make
-      '';
-      installPhase = ''
-        mkdir -p $out/lib
-        cp *.a $out/lib/
-      '';
-      checkPhase = ''
-        make test
-      '';
+            name = "git-lfs-fetch";
+            src = self;
+            buildInputs = [
+              pkgs.clang
+              pkgs.gnumake
+              pkgs.curl
+              pkgs.nlohmann_json
+              pkgs.libgit2
+            ];
+            buildPhase = ''
+              make
+            '';
+            installPhase = ''
+              mkdir -p $out/lib
+              cp *.a $out/lib/
+            '';
+            checkPhase = ''
+              make test
+            '';
+          };
+        });
     };
-        }
-      );
-    };
-  
+
 }
 
